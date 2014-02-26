83cb5770666d71d0e65a981e528bb6f685aec599
Caf131 <christopher.forehand@gmail.com>
2014-02-26 16:06:21 -0500

    Modified Note and Note Element Classes added dependencies to maven for Jackson

--- virtual-classroom/src/main/java/com/vcclass/app/Controllers/HomeController.java
+++ virtual-classroom/src/main/java/com/vcclass/app/Controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.vcclass.app.Controllers;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
