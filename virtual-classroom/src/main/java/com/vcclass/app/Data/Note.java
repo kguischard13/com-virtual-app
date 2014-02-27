8001c72675ad108b7780e9881ca2ea3751cb8670
Kester Guischard <vernanguis@gmail.com>
2014-02-27 18:19:30 -0500

    saddasa

--- virtual-classroom/src/main/java/com/vcclass/app/Data/Note.java
+++ virtual-classroom/src/main/java/com/vcclass/app/Data/Note.java
@@ -5,26 +5,20 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
-<<<<<<< HEAD
 
-
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
-
-
-
-=======
 import org.codehaus.jackson.annotate.JsonProperty;
->>>>>>> FETCH_HEAD
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.context.WebApplicationContext;
 
-<<<<<<< HEAD
+import org.codehaus.jackson.annotate.JsonProperty;
 
-@Service
-=======
->>>>>>> FETCH_HEAD
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.WebApplicationContext;
+
+
+//@Service
+
 @Scope(WebApplicationContext.SCOPE_REQUEST)
 public class Note
 {
