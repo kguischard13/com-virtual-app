69420266dfe1baec95aa919c7c4d28d24ee10c58
Caf131 <christopher.forehand@gmail.com>
2014-03-03 17:35:27 -0500

    Update the NoteDAO and LiveDiscussionDAO
    Create now returns the ID of the newly created note object.  Added the
    SetDataSource method to the DAO

--- virtual-classroom/src/main/java/com/vcclass/app/Services/LiveDiscussionDAO.java
+++ virtual-classroom/src/main/java/com/vcclass/app/Services/LiveDiscussionDAO.java
@@ -1,11 +1,14 @@
 package com.vcclass.app.Services;
 
+import javax.sql.DataSource;
+
 import com.vcclass.app.Data.LiveDiscussion;
 
 public interface LiveDiscussionDAO 
 {
+	public void SetDataSource(DataSource ds); 
 	public LiveDiscussion GetLiveDiscussion(int studentId, int liveDiscussionId, int courseId); 
-	public boolean CreateLiveDiscussion(int studentId, LiveDiscussion discussion); 
+	public int CreateLiveDiscussion(int studentId, LiveDiscussion discussion); 
 	public boolean DeleteLiveDiscussion(int studentId, int liveDiscussionId); 
 	public boolean UpdateLiveDiscussion(int studentId, int liveDiscussionId); 
 }
--- virtual-classroom/src/main/java/com/vcclass/app/Services/NoteDAO.java
+++ virtual-classroom/src/main/java/com/vcclass/app/Services/NoteDAO.java
@@ -2,16 +2,19 @@ package com.vcclass.app.Services;
 
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import com.vcclass.app.Data.Note;
 import com.vcclass.app.Data.NoteElement;
 
 
 public interface NoteDAO 
 {
+	public void SetDataSource(DataSource ds); 
 	public List<Note> GetStudentNotes(int studentId); 
-	public boolean AddNote(int studentId, Note note); 
+	public int AddNote(int studentId, Note note); 
 	public boolean DeleteNote(int studentId, int noteId); 
-	public void EditNote(int studentId, int noteId); 
+	public boolean UpdateNote(int studentId, int noteId); 
 	
 	
 	//public void AddNoteElement(int studentId, Note note); 
