58fdddad51fcc88dc7a12cff69ed9b2877e16324
Kester Guischard <vernanguis@gmail.com>
2014-03-03 23:13:23 -0500

    Update Interface

--- virtual-classroom/src/main/java/com/vcclass/app/Repository/UserDAO.java
+++ virtual-classroom/src/main/java/com/vcclass/app/Repository/UserDAO.java
@@ -6,7 +6,7 @@ import com.vcclass.app.Data.*;
 public interface UserDAO {
 	
 	public User GetUser (int id);
-	public boolean AddUser (User user); // only if we arent having automated ids
+	public int AddUser (User user); // only if we arent having automated ids
 	public boolean DeleteUser (int id);
 	public boolean UpdateUser(int id);
 	public boolean ValidateUser(int id);
