ba00c0f691e3ae4da82b6bad82b3ac53e7513181
Olivia Bradley-Willemann <obradleywillemann@gmail.com>
2014-05-01 00:57:31 -0400

    Complete QuestionController and CommentController

--- virtual-classroom/src/main/java/com/vcclass/app/Controllers/QuestionController.java
+++ virtual-classroom/src/main/java/com/vcclass/app/Controllers/QuestionController.java
@@ -2,43 +2,68 @@ package com.vcclass.app.Controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import com.vcclass.app.Data.Question;
-import com.vcclass.app.Data.Comment;
+import com.vcclass.app.Data.*;
+import com.vcclass.app.Services.*;
 
+import java.util.List;
 
 
 @Controller
 public class QuestionController 
 {
+	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	QuestionService questionService = (QuestionService) context.getBean("questionService");
+	
 	private static final Logger logger = LoggerFactory.getLogger(QuestionController.class); 
 
-	@RequestMapping(value = "/question", method = RequestMethod.GET)
-	public @ResponseBody  Question GetQuestion(Model model)
+	//Add question
+	@RequestMapping(value = "/question/createquestion", method = RequestMethod.POST)
+	public @ResponseBody int CreateQuestion(@PathVariable int userid, @RequestBody Question incquestion)
 	{
-		Question question = new Question(); 
-		
-		question.SetQuestionId(11);
-		question.SetContents("What question is this?");
-		
-		Comment comment = new Comment();
-		
-		comment.SetCommentId(22);
-		comment.SetContents("My comment is that I like this question");
-		
-		question.AddComment(comment);
-		model.addAttribute("Question", question.GetContents()); 
-		model.addAttribute("Comment", comment.GetContents());
-		
-		logger.info("Return the question");
-		
-		return question;
+		Question question = incquestion;
+		int id = questionService.CreateQuestion(userid, incquestion);
+		return id;
+	}
+
+	//Get question
+	@RequestMapping(value = "/question/getquestion/question/{questionid}/user/{userid}/course/{courseid}", 
+			method = RequestMethod.GET)
+	public @ResponseBody  Question GetQuestion(@PathVariable int questionid, @PathVariable int userid,
+			@PathVariable int courseid)
+	{
+		return questionService.GetQuestion(questionid, userid, courseid);
+	}
+	
+	//Get all questions
+	@RequestMapping(value = "/question/getquestions/user/{userid}/course/{courseid}", 
+			method = RequestMethod.GET)
+	public @ResponseBody List<Question> GetQuestions(@PathVariable int userid, @PathVariable int courseid)
+	{
+		return questionService.GetQuestions(userid, courseid);
+	}
+	
+	//Update question
+	@RequestMapping(value = "/question/updatequestion/", method = RequestMethod.POST)
+	public @ResponseBody boolean UpdateQuestion(@RequestBody Question incquestion)
+	{
+		return questionService.UpdateQuestion(incquestion);
+	}
+	
+	//Delete question
+	@RequestMapping(value = "/question/deletequestion/question/{questionid}/user/{userid}",
+			method = RequestMethod.GET)
+	public @ResponseBody boolean DeleteQuestion(@PathVariable int questionid, @PathVariable int userid)
+	{
+		return questionService.DeleteQuestion(questionid, userid);
 	}
 
 }
